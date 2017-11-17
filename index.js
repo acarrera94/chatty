e9c67223661d7b9d7ab010c8ccdba4cd5913ffdf
Jaxon Keeler <jdk_08_09@yahoo.com>
2017-11-17 05:14:43 +0000

    backend linked to mongodb

--- index.js
+++ index.js
@@ -14,17 +14,50 @@ mongo.connect('mongodb://127.0.0.1/mongochat', function(err, db){
 
 app.use(express.static('lib'));
 
-app.get('/', function(req, res){
-res.sendFile(__dirname + '/index.html');
-});
+// app.get('/', function(req, res){
+// res.sendFile(__dirname + '/index.html');
+// });
 
 io.on('connection', function(socket){
-socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
-  });
+    var chat = db.collection('chats');
+
+    sendStatus = (s) => {
+      socket.emit('status', s);
+    }
+
+    chat.find().limit(15).sort({_id:1}).toArray(function(err, res) {
+      if(err) throw err;
+
+      socket.emit('output', res);
+    });
+
+    socket.on('input', function(data){
+      var name = data.name;
+      var message = data.message;
+
+      if(name == '' || message == '')
+        sendStatus('You must enter a name and message');
+      else {
+        chat.insert({name: name, message: message}, function(){
+          io.emit('output', [data]);
+
+          sendStatus({
+            message: "Message sent",
+            clear: true
+          });
+        });
+      }
+    });
+
+    socket.on('clear', function(data){
+      chat.remove({}, function(){
+        socket.emit('cleared');
+      })
+    })
+
+    // io.emit('chat message', msg);
 });
 
 http.listen(3001, function(){
   console.log('listening on *:3001');
 });
-    
